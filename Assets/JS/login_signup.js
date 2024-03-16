97aab0e7ec40ab30014789ea79e0484c4fbff95f
Sarans13 <114057996+Sarans13@users.noreply.github.com>
2024-03-16 12:09:55 +0530

    login-signup

---
+++ Assets/JS/login_signup.js
@@ -0,0 +1,11 @@
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2500,
+    // delay: 300,
+    reset: true
+})
+
+sr.reveal(`.left__container`);
